541872936fa90197b1cae31caee1e00ea711710b
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2018-02-18 15:00:51 +0000

    Port `FieldRef`

---
+++ lunr/field_ref.py
@@ -0,0 +1,24 @@
+from lunr.exceptions import BaseLunrException
+
+
+class FieldRef:
+
+    JOINER = '/'
+
+    def __init__(self, doc_ref, field_name, string_value=None):
+        self.doc_ref = doc_ref
+        self.field_name = field_name
+        self._string_value = string_value
+
+    @classmethod
+    def from_string(cls, string):
+        if cls.JOINER not in string:
+            raise BaseLunrException('Malformed field ref string')
+        field_ref, doc_ref = string.split(cls.JOINER, 1)
+        return cls(doc_ref, field_ref, string)
+
+    def __str__(self):
+        if self._string_value is None:
+            self._string_value = self.field_name + self.JOINER + self.doc_ref
+
+        return self._string_value
