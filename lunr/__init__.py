b5e2f5a171c5592ec7865e3d1a6ed9050071f12c
Yeray Diaz Diaz <yeraydiazdiaz@gmail.com>
2021-10-16 17:56:25 +0100

    0.6.1

--- lunr/__init__.py
+++ lunr/__init__.py
@@ -6,5 +6,5 @@ __all__ = ("lunr", "get_default_builder")
 
 logging.basicConfig(format="%(levelname)-7s -  %(message)s")
 
-__VERSION__ = "0.6.0"
+__VERSION__ = "0.6.1"
 __TARGET_JS_VERSION__ = "2.3.9"
