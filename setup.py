d85b532e0475059eff91ec6fc992667cd7602641
Yeray Diaz <6739793+yeraydiazdiaz@users.noreply.github.com>
2023-06-23 22:20:16 +0100

    Amend setuptools to correctly match required Python 3.7+

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=[],
     extras_require={
         "languages": ["nltk"]
